ef1db99d6832c1b9818d4c1febc596ee78128fb1
koeboe <131195477+koeboe@users.noreply.github.com>
2025-02-19 18:29:00 +0100

    abstracte classes

--- 01-oo/07-abstract-classes/assignments/02-abstract-methods/student.py
+++ 01-oo/07-abstract-classes/assignments/02-abstract-methods/student.py
@@ -1,1 +1,67 @@
 # Write your code here
+from abc import ABC, abstractmethod
+
+
+class A(ABC):
+    def a(self):
+        self.b()
+
+    def e(self):
+        self.c()
+    
+    @abstractmethod
+    def c(self):
+        pass
+
+    @abstractmethod
+    def b(self):
+        pass
+
+class B(A):
+    def b(self):
+        self.a()
+
+    def c(self):
+        self.e()
+
+
+class C(B):
+    def f(self):
+        pass
+
+
+class D(A):
+    def b(self):
+        self.f()
+    
+    @abstractmethod
+    def f(self):
+        pass
+
+
+class E(D):
+    def c(self):
+        self.a()
+
+    def f(self):
+        self.e()
+
+    def g(self):
+        self.f()
+
+
+class F:
+    def a(self):
+        self.b()
+        self.f()
+    
+    def b(self):
+        pass
+
+    def f(self):
+        pass
+    
+
+
+f = F()
+print(f)
